8af2826240d21fb18cc65362e7584d51d9a60520
Matthew Duong <matt@timeweave.com.au>
2015-09-19 17:12:03 +1000

    Changed schema

--- schema.sql
+++ schema.sql
@@ -19,9 +19,7 @@ CREATE TABLE `users` (
 	`id` int(11) NOT NULL AUTO_INCREMENT,
 	`created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	`email` varchar(300) not null,
-	`mobile` varchar(15) not null,
 	UNIQUE(`email`),
-	UNIQUE(`mobile`),
 	PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
