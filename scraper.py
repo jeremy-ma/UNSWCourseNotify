a6b56b653cda9e1eae70c2eb2477ec2a8bab9b05
jeremy-ma <jeremy_ma@outlook.com>
2015-09-19 12:59:24 +1000

    can scrape a subject

--- scraper.py
+++ scraper.py
@@ -1,27 +1,78 @@
 from bs4 import BeautifulSoup
 import requests
+import pdb
+import re
 
-url = 'http://classutil.unsw.edu.au/ELEC_S2.html'
 
-r = requests.get(url)
-data = r.text
+def scrape_subject(url):
 
-soup = BeautifulSoup(data)
+    r = requests.get(url)
+    data = r.text
+    soup = BeautifulSoup(data)
 
-for section in soup.find_all(class_="cucourse"):
-    element = section.find("b")
-    if element is not None:
-        print element.find_all(text=True)[0].rstrip()
+    regex = re.compile(r'(\w{3} [-:\d]+)')
+    sectionlist = []
+
+    for section in soup.find_all(class_="cucourse"):
+        element = section.find("b")
+        if element is not None:
+            # pdb.set_trace()
+            #print element.find_all(text=True)[0].rstrip()
+            code = element.text.rstrip()
+            name = section.findNext('td').text
+            row = section.findNext('tr')
+
+            while row != None and check_end(row) is False:
+
+                columns = row.findAll("td")
+
+                type_ = columns[0].text
+                section_id = columns[1].text
+                status = columns[4].text
+                enrolled, capacity = [int(x) for x in columns[5].text.split(' ')[0].split('/')]
+
+                timematch = regex.match(columns[7].text)
+                if timematch is not None:
+                    time = timematch.group(1)
+                else:
+                    time = columns[7].text
+
+                section_dict = {}
+                section_dict['course_code'] = code
+                section_dict['course_name'] = name
+                section_dict['id'] = section_id
+                section_dict['type'] = type_
+                section_dict['status'] = status
+                section_dict['num_enrolled'] = enrolled
+                section_dict['capacity'] = capacity
+                section_dict['time'] = time
+
+                print section_dict
+
+                row = row.findNext('tr')
+
+
+
+
+
+def check_end(row):
+    # check the row if its past the end of the subject
+    clas = row.get('class')
+    if clas != None:
+        clas = clas[0]
+    if clas!= 'rowHighlight' and clas != 'rowLowLight':
+        return True
+    elif row.find('a') is not None and  row.find('a').get('href') == '#top':
+        # check for the '^ top ^' hyperlink
+        return True
     else:
-        print section.text
+        return False
 
 
-    nextrow = section.findNext('tr')
-    element = nextrow.find("cucourse")
-    if element is not None:
-        continue
 
-    columns = nextrow.findAll("td")
+if __name__ == '__main__':
 
-    for col in columns:
-        print col
+    url = 'http://classutil.unsw.edu.au/ELEC_S2.html'
+
+    scrape_subject(url)
+
