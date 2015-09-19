952063f62444e04652a148a5e606f1081361094d
Matthew Duong <matt@timeweave.com.au>
2015-09-19 12:25:59 +1000

    Added injection of dom

--- public_html/js/unswcn.js
+++ public_html/js/unswcn.js
@@ -6,6 +6,16 @@ $(document).on('ready',function(){
 var app=function(){
 	this.init=function(){
 		console.log('app initialised');
+		var htmlString=[
+			'<div id="app">',
+				'<div class="header">',
+					'<span>UNSW class notify</span>',
+				'</div>',
+				'<div class="scrollableView">',
+				'</div>',
+			'</div>',
+		].join('');
+		$('body').append(htmlString);
 	}
 	this.init();
 }
