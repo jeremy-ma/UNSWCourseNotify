1e106ec4ef614aba54074bc3d70f04f7a96c0a87
Matthew Duong <matt@timeweave.com.au>
2015-09-19 11:25:33 +1000

    Created api.js

---
+++ api.js
@@ -0,0 +1,19 @@
+console.log('\n\n\n---STARTING SERVER\n\n\n'); //clear console everytime restarts
+
+var express = require('express');
+
+var bodyParser = require('body-parser');
+
+var validator = require('validator');
+
+var app = express();
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+
+
+
+
+
+app.listen(6969);
